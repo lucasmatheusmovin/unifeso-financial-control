738e741a36f606e65c5ab8bbf8b2b0cd063cb54a
lucasmatheusmovin <57500481+lucasmatheusmovin@users.noreply.github.com>
2020-10-13 20:54:34 -0300

    av1 respostas

--- index.js
+++ index.js
@@ -25,42 +25,58 @@ app.use(express.urlencoded({ extended: true })); // for parsing application/x-ww
 app.post("/", async (request, response) => {
   const jsonContent = request.body;
   // User -> objeto responsável por acessar o banco de dados.
-  const user = await User.create(jsonContent);
-  response.status(201).send(user);
+  try{
+    const user = await User.create(jsonContent);
+    response.status(201).send(user);
+  }
+  catch {
+    response.status(400).json({'Erro': 'Usuário não foi cadastrado.'});
+  }
+
 });
 
 // READ
 app.get("/:id", (request, response) => {
-  const obj = dictionary[request.params.id];
-  if (!obj) {
-    return response.status(404).send({
-      message: "Not found"
-    });
-  }
-  response.status(200).send(obj);
+  User.findById(request.params.id, (err, user) => {
+    if(err) {
+      return response.status(404).send({
+        message: "Usuário com este ID não foi encontrado."
+      })
+    } else {
+      return response.status(201).send(user);
+    }
+  });
 });
 
 // UPDATE
 app.put("/:id", (request, response) => {
-  const obj = request.body;
-  if (!dictionary[request.params.id]) {
-    return response.status(404).send({
-      message: "Usuário não encontrado."
-    });
-  }
-  dictionary[request.params.id] = obj;
-  response.status(200).send(obj);
+  const updatedObj = request.body;
+
+  User.findByIdAndUpdate(request.params.id, updatedObj, (err, user) => {
+    if(err) {
+      return response.status(404).send({
+        message: "Usuário não encontrado."
+      }) 
+    } else {
+      return response.status(201).send({
+        message: "Cadastro alterado com sucesso.",
+        user: user
+      })
+    }
+  });
 });
 
 // DELETE
-app.delete("/:id", (request, response) => {
-  if (!dictionary[request.params.id]) {
-    return response.status(404).send({
-      message: "Usuário não encontrado."
-    });
-  }
-  dictionary[request.params.id] = null;
-  response.status(204).end();
+app.delete("/:id", async (request, response) => {
+  const { id } = request.params;
+
+  User.findByIdAndRemove(id, (err, doc) => {
+    if(!err) {
+      response.status(204).end();
+    } else {
+      response.status(404).json({'Erro': 'Usário não foi encontrado'});
+    }
+  });
 });
 
 const port = 8090;
